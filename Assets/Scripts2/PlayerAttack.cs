6b7b4a2597e13458e594f514fa2488f0b761249e
SeongHeeYoon <younsnow8281@gmail.com>
2026-07-13 18:23:39 +0900

    new_ADD_20260713

---
+++ Assets/Scripts2/PlayerAttack.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    public GameObject projectilePrefab;
+    public Transform firePoint;
+    public Transform enemyTarget;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (enemyTarget != null)
+            {
+                ThrowProjectile();
+            }
+        }
+    }
+
+    void ThrowProjectile()
+    {
+        GameObject projectile = Instantiate(
+            projectilePrefab,
+            firePoint.position,
+            Quaternion.identity
+        );
+
+        Projectile projectileScript = projectile.GetComponent<Projectile>();
+
+        Vector2 direction =
+            (enemyTarget.position - firePoint.position).normalized;
+
+        projectileScript.SetDirection(direction);
+    }
+}
